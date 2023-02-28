f67dccb0a6dcb863f19ac5554cc658825477c696
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-27 21:36:32 -0500

    Enum

---
+++ src/Core/Enums/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Enums
+{
+    public enum TicketStatus
+    {
+        Unknown = 0,
+        New = 1,
+        Acknowledged = 2,
+        Investigation = 3,
+        Completed = 4
+    }
+}
