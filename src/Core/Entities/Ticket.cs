9b87f109a0a073194d3003f9f07b5592a12062a1
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-27 21:36:23 -0500

    Business entities for app

---
+++ src/Core/Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using Core.Enums;
+
+namespace Core.Entities
+{
+    public class Ticket : BaseEntity
+    {
+        /// <summary>
+        ///     Name of support ticket.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Ticket status.
+        /// </summary>
+        public TicketStatus Status { get; set; }
+
+        /// <summary>
+        ///     User who is assigned to this ticket.
+        /// </summary>
+        public string Assignee { get; set; }
+    }
+}
---
+++ src/Core/Entities/WikiPage.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Entities
+{
+    public class WikiPage : BaseEntity
+    { 
+        /// <summary>
+        ///     Name of the wiki page.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Description of the wiki page.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///     Content of the wiki page.
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
