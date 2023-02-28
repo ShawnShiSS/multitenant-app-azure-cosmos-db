eaa08dcb341cf283c529bfc58d4d0bcc514ae6e1
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-27 21:35:59 -0500

    Base entity in core

---
+++ src/Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        [JsonProperty(PropertyName = "id")]
+        public virtual string Id { get; set; }
+    }
+}
