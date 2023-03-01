53aaf4a20fc4c7be222bbcf5e5fb4630068544f5
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-28 22:56:10 -0500

    db setup types

---
+++ src/Infrastructure/Persistence/CosmosDb/Config/CosmosDbSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Persistence.CosmosDb
+{
+    public class CosmosDbSettings
+    {
+        /// <summary>
+        ///     CosmosDb Account - The Azure Cosmos DB endpoint
+        /// </summary>
+        public string EndpointUrl { get; set; }
+        /// <summary>
+        ///     Key - The primary key for the Azure DocumentDB account.
+        /// </summary>
+        public string PrimaryKey { get; set; }
+        
+    }
+
+}
---
+++ src/Infrastructure/Persistence/CosmosDb/Constants/CosmosDbConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Persistence.CosmosDb
+{
+    /// <summary>
+    ///     Constants used for Cosmos DB setup.
+    /// </summary>
+    public class CosmosDbConstants
+    {
+        public static readonly string DatabaseName = "TicketTrackingPlatform";
+
+        public static readonly string DataContainerName = "Data";
+        public static readonly string DataContainerPartitionKey = "/TenantId";
+
+        public static readonly string DefaultTenantId = "644d71f1-52c7-4423-a6a2-464323654370";
+    }
+}
