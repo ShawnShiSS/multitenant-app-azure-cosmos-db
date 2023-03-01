3c2e3ddaded617918f0e29fe4eb8d940fc0d1e31
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-28 22:55:06 -0500

    Repository pattern

---
+++ src/Core/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Persistence
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        ///     Get one item by Id
+        /// </summary>
+        Task<T> GetItemAsync(string id);
+
+        Task AddItemAsync(T item);
+        
+        Task UpdateItemAsync(string id, T item);
+        
+        Task DeleteItemAsync(string id);
+
+        /// <summary>
+        ///     Get items by providing a query string.
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetItemsAsync(string queryString);
+    }
+}
