fb3856ad3a83f16ecfc3a5caf7b13c90f97f66b1
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-28 22:56:26 -0500

    Extension methods useful for application startup.

---
+++ src/Infrastructure/Persistence/CosmosDb/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Core.Entities;
+using Core.Interfaces.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Persistence.CosmosDb.Extensions
+{
+    /// <summary>
+    ///     Extension methods for application builder. 
+    /// </summary>
+    public static class IApplicationBuilderExtensions
+    {
+        /// <summary>
+        ///     Ensure Cosmos DB is created
+        /// </summary>
+        /// <param name="builder"></param>
+        public static async Task EnsureCosmosDbIsCreated(this IApplicationBuilder builder)
+        {
+            using (IServiceScope serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                // This should be a singleton instance
+                Microsoft.Azure.Cosmos.CosmosClient cosmosClient = serviceScope.ServiceProvider.GetService<Microsoft.Azure.Cosmos.CosmosClient>();
+
+                // Create the database
+                Microsoft.Azure.Cosmos.DatabaseResponse database = await cosmosClient.CreateDatabaseIfNotExistsAsync(CosmosDbConstants.DatabaseName);
+
+                // Create the container(s)
+                await database.Database.CreateContainerIfNotExistsAsync(CosmosDbConstants.DataContainerName, CosmosDbConstants.DataContainerPartitionKey);
+            }
+        }
+
+        /// <summary>
+        ///     Seed sample data in the Data container
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static async Task SeedDataContainerIfEmptyAsync(this IApplicationBuilder builder)
+        {
+            using (IServiceScope serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                ITicketRepository _repo = serviceScope.ServiceProvider.GetService<ITicketRepository>();
+
+                // Check if empty
+                string sqlQueryText = "SELECT * FROM c";
+                IEnumerable<Ticket> tickets = await _repo.GetItemsAsync(sqlQueryText);
+
+                if (tickets != null && tickets.Count() > 0)
+                {
+                    return;
+                }
+
+                // Seed
+                string defaultTenantId = CosmosDbConstants.DefaultTenantId;
+
+                await _repo.AddItemAsync(new Ticket()
+                {
+                    TenantId = defaultTenantId,
+                    Name = "Default ticket",
+                    Status = Core.Enums.TicketStatus.New
+                });
+
+                await _repo.AddItemAsync(new Ticket()
+                {
+                    TenantId = defaultTenantId,
+                    Name = "Default ticket 2",
+                    Status = Core.Enums.TicketStatus.Acknowledged
+                });
+            }
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/CosmosDb/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Persistence.CosmosDb.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Register a singleton instance of CosmosClient.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="endpointUrl"></param>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services,
+                                                     string endpointUrl,
+                                                     string primaryKey)
+        {
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(endpointUrl, primaryKey);
+            
+            // Microsoft recommends a singleton client instance to be used throughout the application
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.cosmos.cosmosclient?view=azure-dotnet#definition
+            // "CosmosClient is thread-safe. Its recommended to maintain a single instance of CosmosClient per lifetime of the application which enables efficient connection management and performance"
+            services.AddSingleton<Microsoft.Azure.Cosmos.CosmosClient>(client);
+
+            return services;
+        }
+    }
+}
